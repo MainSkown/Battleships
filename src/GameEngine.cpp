b9dd83b97272c03a14d9d880e655dca0372e242f
MainSkown <mainskown.contact@gmail.com>
2023-05-07 14:47:47 +0200

    Added Tileset

--- src/GameEngine.cpp
+++ src/GameEngine.cpp
@@ -10,7 +10,7 @@ GameEngine::GameEngine(const char *title, int xpos, int ypos, int width, int hei
 
     renderer = SDL_CreateRenderer(window, -1, 0);
     if(renderer){
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     }
 
     isRunning = true;
