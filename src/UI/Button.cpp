11372110bd259056fbb3119f1a60c912ab6c2460
MainSkown <mainskown.contact@gmail.com>
2023-05-08 18:37:53 +0200

    Made button usefull

--- src/UI/Button.cpp
+++ src/UI/Button.cpp
@@ -1,17 +1,24 @@
 #include "Button.h"
 #include "../TextureManager.h"
 
-Button::Button(const char *path, int x, int y, int w, int h) {
+Button::Button(const char *path, int x, int y, int w, int h, std::function<void()> onClick) {
     texture = TextureManager::LoadTexture(path);
     srcRect.x = srcRect.y = 0;
     srcRect.w = destRect.w = w;
     srcRect.h = destRect.h = h;
     destRect.x = x;
     destRect.y = y;
+    this->onClick = onClick;
 }
 
 void Button::HandleEvent(const SDL_Event *event) {
-
+    if(event->type == SDL_MOUSEBUTTONDOWN){
+        int x,y;
+        SDL_GetMouseState(&x, &y);
+        if(x >= destRect.x && x <= destRect.x + destRect.w && y >= destRect.y && y <= destRect.y + destRect.h){
+            onClick();
+        }
+    }
 }
 
 void Button::update() {
--- src/UI/Button.h
+++ src/UI/Button.h
@@ -2,12 +2,15 @@
 #define BATTLESHIPS_BUTTON_H
 
 #include <SDL.h>
+#include <functional>
 #include "UI.h"
 #include "UIElements.h"
 
 class Button : public UIElement {
+private:
+    std::function<void()> onClick;
 public:
-    Button(const char* path, int x, int y, int w, int h);
+    Button(const char* path, int x, int y, int w, int h, std::function<void()> onClick);
     void HandleEvent(const SDL_Event* event) override;
     void update() override;
     std::vector<SDL_EventType> getAllEventsUsed() override;
