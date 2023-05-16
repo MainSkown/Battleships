9d9a9fcb7aae614e0d52348380840cdaf6412e0f
MainSkown <mainskown.contact@gmail.com>
2023-05-17 00:54:39 +0200

    Upgraded AI

--- src/BattleshipAI.cpp
+++ src/BattleshipAI.cpp
@@ -1,8 +1,9 @@
 #include "BattleshipAI.h"
+#include <random>
 
 void BattleshipAI::init() {
-    for(int i = 0; i < 10; i++){
-        for(int j = 0; j < 10; j++){
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
             map[i][j] = {false, false};
         }
     }
@@ -11,69 +12,57 @@ void BattleshipAI::init() {
 void BattleshipAI::Shoot(Map &playerMap) {
     std::vector<TileAI> tileset;
     // Add points
-    for(int i = 0; i < 10; i++){
-        for(int j = 0; j < 10; j++){
-            if(map[i][j].first){
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (map[i][j].first) {
                 continue;
+            }
+
+            if (i + 1 < 10 && j + 1 < 10) {
+                if (map[i + 1][j + 1].second) {
+                    continue;
+                }
+            }
+            if (i - 1 >= 0 && j + 1 < 10) {
+                if (map[i - 1][j + 1].second) {
+                    continue;
+                }
+            }
+            if (i + 1 < 10 && j - 1 >= 0) {
+                if (map[i + 1][j - 1].second) {
+                    continue;
+                }
+            }
+            if (i - 1 >= 0 && j - 1 >= 0) {
+                if (map[i - 1][j - 1].second) {
+                    continue;
+                }
             }
 
             TileAI t;
             t.x = j;
             t.y = i;
-            if(i - 1 >= 0){
-                if(map[i - 1][j].second){
+            if (i - 1 >= 0) {
+                if (map[i - 1][j].second) {
                     t += 5;
-                }
-                if(map[i - 1][j].first){
-                    t += 2;
                 }
             }
 
-            if(j - 1 >= 0){
-                if(map[i][j - 1].second){
+            if (j - 1 >= 0) {
+                if (map[i][j - 1].second) {
                     t += 5;
-                }
-                if(map[i][j - 1].first){
-                    t += 2;
                 }
             }
 
-            if(i + 1 < 10){
-                if(map[i + 1][j].second){
+            if (i + 1 < 10) {
+                if (map[i + 1][j].second) {
                     t += 5;
-                }
-                if(map[i + 1][j].first){
-                    t += 2;
                 }
             }
 
-            if(j + 1 < 10){
-                if(map[i][j - 1].second){
+            if (j + 1 < 10) {
+                if (map[i][j - 1].second) {
                     t += 5;
-                }
-                if(map[i][j - 1].first){
-                    t += 2;
-                }
-            }
-
-            if(i + 1 < 10 && j + 1 < 10){
-                if(map[i + 1][j + 1].second){
-                    t = 0;
-                }
-            }
-            if(i - 1 >= 0 && j + 1 < 10){
-                if(map[i - 1][j + 1].second){
-                    t = 0;
-                }
-            }
-            if(i + 1 < 10 && j - 1 >= 0){
-                if(map[i + 1][j + 1].second){
-                    t = 0;
-                }
-            }
-            if(i - 1 >= 0 && j - 1 >= 10){
-                if(map[i + 1][j + 1].second){
-                    t = 0;
                 }
             }
 
@@ -83,6 +72,26 @@ void BattleshipAI::Shoot(Map &playerMap) {
 
     std::sort(tileset.begin(), tileset.end(), [](auto x, auto y) { return x > y; });
 
-    map[tileset[0].y][tileset[0].x].second = playerMap.Shoot(tileset[0].x, tileset[0].y);
-    map[tileset[0].y][tileset[0].x].first = true;
+    if (!tileset.empty()) {
+        if (tileset[0].value == 0) {
+            // Pick on random (if not hit)
+            std::random_device rd;
+            std::mt19937 gen(rd());
+
+            std::uniform_int_distribution<> dis(0, tileset.size() - 1);
+            int index = dis(gen);
+            int x = tileset[index].x;
+            int y = tileset[index].y;
+
+            map[y][x].second = playerMap.Shoot(x, y);
+            map[y][x].first = true;
+            return;
+
+        }
+    }
+
+    if (tileset[0].value != 0) {
+        map[tileset[0].y][tileset[0].x].second = playerMap.Shoot(tileset[0].x, tileset[0].y);
+        map[tileset[0].y][tileset[0].x].first = true;
+    }
 }
