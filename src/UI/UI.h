6b9b5a138d0552dc7dc698af096baf9ff051a88c
MainSkown <mainskown.contact@gmail.com>
2023-05-17 22:52:32 +0200

    Fixed segmentation fault

--- src/UI/UI.h
+++ src/UI/UI.h
@@ -6,8 +6,10 @@
 #include <bitset>
 #include <memory>
 #include <algorithm>
+#include <iostream>
 
 class UILayer;
+
 class UIElement;
 
 using EventBitset = std::bitset<32>;
@@ -18,33 +20,45 @@ protected:
     SDL_Texture *texture;
 
 public:
-    virtual void HandleEvent(const SDL_Event* event) = 0;
+    bool active = true;
+
+    virtual void HandleEvent(const SDL_Event *event) = 0;
+
     virtual void update() = 0;
+
     virtual std::vector<SDL_EventType> getAllEventsUsed() = 0;
+
     void draw();
+
     virtual ~UIElement();
+
     virtual void clean();
 };
 
 class UILayer {
-    private:
-        std::vector<std::unique_ptr<UIElement>> elements;
+private:
+    std::vector<std::unique_ptr<UIElement>> elements;
 
-        EventBitset eventTypes;
-    public:
-    void update() {for(auto &c : elements) c->update();}
-    void draw(){ for (auto& element : elements) element->draw(); };
-    void HandleEvent(const SDL_Event* event) {for(auto &c : elements) c->HandleEvent(event);}
+    EventBitset eventTypes;
+public:
+    void update() { for (auto &c: elements) c->update(); }
+
+    void draw() { for (auto &element: elements) if (element->active)element->draw(); };
+
+    void HandleEvent(const SDL_Event *event) {
+        // Must be reversed because of the element may be deleted in the middle of the loop
+        for (int i = elements.size() - 1; i >= 0; i--) if (elements[i]->active) elements[i]->HandleEvent(event);
+    }
 
     template<typename T, typename... tArgs>
-    T& AddElement(tArgs&&... mArgs){
-        T* element(new T(std::forward<tArgs>(mArgs)...) );
+    T &AddElement(tArgs &&... mArgs) {
+        T *element(new T(std::forward<tArgs>(mArgs)...));
         std::unique_ptr<UIElement> uPtr{element};
 
         elements.emplace_back(std::move(uPtr));
 
         auto events = element->getAllEventsUsed();
-        for(auto e : events){
+        for (auto e: events) {
             eventTypes[e] = true;
         }
 
@@ -56,9 +70,10 @@ class UILayer {
         return eventTypes[eT];
     }
 
-    void DeleteElement(UIElement* element) {
+    void DeleteElement(UIElement *element) {
         auto el =
-                std::find_if(elements.begin(), elements.end(), [&](std::unique_ptr<UIElement> &e) { return e.get() == element; });
+                std::find_if(elements.begin(), elements.end(),
+                             [&](std::unique_ptr<UIElement> &e) { return e.get() == element; });
         el->reset();
         elements.erase(std::remove(elements.begin(), elements.end(), *el), elements.end());
     }
